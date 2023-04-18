ea105ee18beea1d42735e7060a31411d83a892fc
cl0ud <123465932+cl0udstar@users.noreply.github.com>
2023-04-18 12:04:05 +0100

    Update quiz-script.js

--- scripts/quiz-script.js
+++ scripts/quiz-script.js
@@ -9,27 +9,11 @@ function displayFlag() {
     flag_contents2.style.cursor = "pointer";
 }
 
+
 function copyFlag() {
     navigator.clipboard.writeText(secret);
 }
 
-// function secretFind() {
-//     var check_box_hint1 = document.getElementById("hint1-check");
-//     var check_box_hint2 = document.getElementById("hint2-check");
-//     var check_box_hint3 = document.getElementById("hint3-check");
-//     var check_box_hint4 = document.getElementById("hint4-check");
-//     var check_box_hint5 = document.getElementById("hint5-check");
-    
-//     if (check_box_hint1.checked == true && check_box_hint2.checked == true && check_box_hint3.checked == false && check_box_hint4.checked == true && check_box_hint5.checked == false) {
-//         var secret_recipe = prompt("---Hopefully you have the flag copied!---\n--If not keep searching. (at least you found the prompt)");
-//         if (secret_recipe == secret) {
-//             const url = "recipes/secret-recipe-pizza.html";
-//             window.open(url, "_self"); 
-//         } else {
-//             location.reload();
-//         }
-//     }
-// }
 
 function spoiler() {
     const key = document.getElementById("hide-key");
@@ -38,6 +22,7 @@ function spoiler() {
     key.style.color = "#FEFFE2";
     key.style.cursor = "default";
 }
+
 
 var check_box_hint1 = document.getElementById("hint1-check");
 var check_box_hint2 = document.getElementById("hint2-check");
@@ -68,6 +53,7 @@ function displayPrompt() {
         p_box.style.visibility = "hidden";
     }
 }
+
 
 function checkKey() {
     var inputValue = document.getElementById("key-text").value;
